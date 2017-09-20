3f0829153b7f84e04f76dc0fc177a34e4f1163d6
Chester Lai (RD-TW) <waiyu.lai@gmail.com>
2017-09-20 19:09:25 +0800

    initial

---
+++ enable_nagios_alert.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# This is a sample shell script showing how you can submit the ENABLE_HOST_SVC_NOTIFICATIONS command
+# to Nagios.  Adjust variables to fit your environment as necessary.
+
+now=`date +%s`
+commandfile='/usr/local/nagios/var/rw/nagios.cmd'
+
+for host in `cat hosts`;
+do /usr/bin/printf "[%lu] ENABLE_HOST_SVC_NOTIFICATIONS;$host\n" $now > $commandfile;
+/usr/bin/printf "[%lu] ENABLE_HOST_NOTIFICATIONS;$host\n" $now > $commandfile;
+done
